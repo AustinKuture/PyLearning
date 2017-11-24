ec3c43046129745ba051366c3d1fd33c84b3a4df
Kuture <austinkuture@126.com>
2017-11-24 08:35:02 +0800

    新增整个网站爬取的功能

--- AKMain.py
+++ AKMain.py
@@ -1,42 +1,28 @@
+#coding=utf-8
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+# 主程序入口
+def download_html(url):
+
+    url_data = urlopen(url)
+
+    bs_obj = BeautifulSoup(url_data, 'lxml')
+
+    for link in bs_obj.findAll('a', href=re.compile('^(/wiki/)((?!:).)*$')):
+
+        link = link.find('div', {'id', 'bodyContent'})
+
+        if 'href' in link.attrs:
+
+            print(link.attrs['href'])
 
 
 
 
+# download_html('http://www.pythonscraping.com/pages/warandpeace.html')
+# download_html('http://www.pythonscraping.com/pages/page3.html')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+download_html('http://en.wikipedia.org/wiki/Kevin_Bacon')
---
+++ AKScapWebSite00.py
@@ -0,0 +1,77 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from time import sleep
+import re
+
+# wf = open('cocochina.txt','w')
+
+pages = set()
+def getLinks(pageUrl):
+
+    try:
+
+        global pages
+        html = urlopen('http://www.cocoachina.com' + pageUrl)
+        bs_obj = BeautifulSoup(html, 'lxml')
+
+        for link in bs_obj.findAll('a',href=re.compile('^(/ios/)')):
+
+            print(link)
+            if 'href' in link.attrs:
+
+                if link.attrs['href'] not in pages:
+
+                    newPage = link.attrs['href']
+                    print(newPage)
+
+                    wf.write(newPage)
+                    pages.add(newPage)
+                    getLinks(newPage)
+
+    except Exception as error:
+
+        print(error)
+
+    finally:
+
+        wf.close()
+
+                # sleep(0.001)
+getLinks('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
